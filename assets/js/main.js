2570222a8a90f44a87f101ec42db8e04dc54418b
Muhammad Chairul <email.muhammad.chairul@gmail.com>
2022-11-27 05:22:51 +0700

    add btn hire me

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function($) {
     $('.level-label').tooltip();
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "mchairul");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
@@ -31,3 +31,32 @@ jQuery(document).ready(function($) {
 
 
 });
+
+const sendMsg = () => {
+    let name = $('#name').val();
+    let contact = $('#contact').val();
+    let req = $('#req').val();
+
+    if(name == '' || contact == '' || req == '') {
+        alert('please complete all inputs!');
+    } else {
+        $("#btnSubmit").hide();
+        $("#btnLoader").show();
+        $.ajax({
+            type: "GET",
+            url: "https://gadaibpkbmobil.my.id/tele/send_notif_irul.php",
+            crossDomain: true,
+            data: 'sender=' + name + '&contact=' + contact + '&req=' + req,
+            success: function (data) {
+                alert('thank you, i will contact you as soon as possible');
+                $('#hireMe').hide();
+                $('#modalHire').modal('hide')
+            },
+            error: function (err) {
+                alert('something went wrong, please try again!');
+                $("#btnSubmit").show();
+                $("#btnLoader").hide();
+            }
+        });
+    }
+}
